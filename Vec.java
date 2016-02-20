9ea57febdf13f7915ad55b381059b8d8b5a6eac2
naancents <naancents@users.noreply.github.com>
2016-02-20 04:34:48 -0500

    Added files via upload

--- Vec.java
+++ Vec.java
@@ -1,5 +1,17 @@
 public class Vec{
 	private double x,y;
+	public double getX() {
+		return x;
+	}
+	public void setX(double x) {
+		this.x = x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) {
+		this.y = y;
+	}
 	public Vec(){
 	}
 	public Vec(Vec v){
