f0c2e71c840d7c60ce9d22643a2fcd6cc458e08b
Vikram Rao <vikzuni@gmail.com>
2016-02-20 06:37:45 -0500

    Updated collisions.java, Vec.java

--- SweetDreams/src/sweetdreams/Vec.java
+++ SweetDreams/src/sweetdreams/Vec.java
@@ -16,4 +16,13 @@ public class Vec{
 	public static Vec mult(double c, Vec v1){
 		return new Vec(c*v1.x,c*v1.y);
 	}
+	public static double mag(Vec v1){
+		return Math.sqrt(Math.pow(v1.x, 2)+Math.pow(v1.y, 2));
+	}
+	public static double dot(Vec v1, Vec v2){
+		return v1.x*v2.x+v1.y+v2.y;
+	}
+	public static Vec proj(Vec v1, Vec v2){
+		return Vec.mult(Vec.dot(v1, v2)/Vec.dot(v2,v2),v2);
+	}
 }
