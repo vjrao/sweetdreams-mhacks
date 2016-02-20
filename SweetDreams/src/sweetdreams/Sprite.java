02ffb2a34e7c24c86585ab808f45446d0bae89f1
BlueKeleher <annablue@gmail.com>
2016-02-20 06:06:04 -0500

    Character sprite virus basic

--- SweetDreams/src/sweetdreams/Sprite.java
+++ SweetDreams/src/sweetdreams/Sprite.java
@@ -1,5 +1,19 @@
 package sweetdreams;
 
 public class Sprite {
-
+	private int x, y;
+	
+	public Sprite(int startX, int startY) {
+		x = startX;
+		y = startY;
+	}
+	
+	public void move(int dx, int dy) {
+		x += dx;
+		y += dy;
+	}
+	
+	public int getX() { return x; }
+	public int getY() { return y; }
+	
 }
--- SweetDreams/src/sweetdreams/Virus.java
+++ SweetDreams/src/sweetdreams/Virus.java
@@ -1,5 +1,16 @@
 package sweetdreams;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 public class Virus extends Sprite {
 
+	public Virus(int startX, int startY) {
+		super(startX, startY);
+	}
+	
+	public void draw(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+	}
+	
 }
